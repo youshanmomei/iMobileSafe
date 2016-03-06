4471af9a528e46d50ed2506af7e0c58414679fc7
qc <1142041743@qq.com>
2016-03-06 15:15:37 +0800

    搭建架构

--- app/src/main/java/com/huaiying/imobilesafe/ui/activity/MainActivity.java
+++ app/src/main/java/com/huaiying/imobilesafe/ui/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.huaiying.imobilesafe;
+package com.huaiying.imobilesafe.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.huaiying.imobilesafe.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/huaiying/imobilesafe/util/EncryptionUtils.java
@@ -0,0 +1,26 @@
+package com.huaiying.imobilesafe.util;
+
+public class EncryptionUtils {
+
+	public static String encode(String text, int key) {
+
+		byte[] bytes = text.getBytes();
+
+		for (int i = 0; i < bytes.length; i++) {
+			// bytes[i] = xxx;
+			bytes[i] ^= key;
+		}
+		return new String(bytes);
+	}
+	
+	public static String decode(String text, int key) {
+
+		byte[] bytes = text.getBytes();
+
+		for (int i = 0; i < bytes.length; i++) {
+			// bytes[i] = xxx;
+			bytes[i] ^= key;
+		}
+		return new String(bytes);
+	}
+}
